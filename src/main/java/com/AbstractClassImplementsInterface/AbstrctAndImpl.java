a2f2719e7a9bbc0fb28458b88b53df5ed426be65
nikolazmailz <nikolazmailz@gmail.com>
2017-07-19 15:42:04 +0500

    commit MainClass JavaSE

---
+++ src/main/java/com/AbstractClassImplementsInterface/AbstrctAndImpl.java
@@ -0,0 +1,10 @@
+package com.AbstractClassImplementsInterface;
+
+/**
+ * Created by nikk on 19.07.2017.
+ */
+public abstract class AbstrctAndImpl implements Intrfc {
+
+    abstract String MakeNoise();
+
+}
